09fbea9d80e68a5bcbc2447557e2b4503a1e8694
Salacas <lean.ricciardi@gmail.com>
2022-04-15 23:23:20 -0300

    hasta paso 4.1 inclusive

--- consola/include/consola.h
+++ consola/include/consola.h
@@ -1,16 +1,16 @@
 /*
- * consola_sys32.h
+ * consola.h
  *
  *  Created on: 14 abr. 2022
  *      Author: utnso
  */
 
-#ifndef CONSOLA_SYS32_H_
-#define CONSOLA_SYS32_H_
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
 
 
-#endif /* CONSOLA_SYS32_H_ */
+#endif /* CONSOLA_H_ */
---
+++ consola/src/consola.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : consola.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "consola.h"
+
+int main(int argc, char* argv[]) {
+
+	char *ip, *port;
+	//Etapa de inicializacion. Arrancamos log, config, pseudocode y conectamos con Kernel.
+	crear_logger();
+	t_config* config = cargar_config();
+
+	if(argc != 3){
+		log_error(logger, "No se hay ingresado la cantidad de parametros correcta. Terminando ejecucion.");
+	}
+	log_info(logger, "Se han ingresado 2 parametros.");
+	log_info(logger, "Filepath [Parametro 1]");
+	log_info(logger, argv[1]);
+	log_info(logger, "Tamanio memoria [Parametro 2]");
+	log_info(logger, argv[2]);
+
+	ip = leer_valor_config(config, "IP_KERNEL");
+	port = leer_valor_config(config, "PUERTO_KERNEL");
+	config_destroy(config);
+
+
+	log_destroy(logger);
+	return EXIT_SUCCESS;
+}
